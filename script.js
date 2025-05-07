7a0ccfd6bf10eccbfb8ef0295a5c603168616c30
Nikhil6791 <nikhilspandey08@gmail.com>
2025-05-07 08:22:41 +0530

    Added new features

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
 let userTask = document.querySelector(".task");
 let button = document.getElementById("add-task");
-console.log(addTask);
+let container = document.querySelector(".container");
+console.log(button);
 
 let taskContainer = document.querySelector(".task-container");
-
+let taskAdded = document.querySelector(".task-added");
 
 let circleId = 0;
 let crossID = 0;
 
-button.addEventListener('click', (e) =>{
-    circleId++;
-    crossID++;
-    let taskContent = document.createElement('div')
-    taskContent.setAttribute("class", "task-container")
-    console.log(taskContent);
-    taskContent.innerHTML = `<div id="${circleId}" class="circle">
-                    <img class="tick" src="tick.svg" alt="">
+button.addEventListener("click", (e) => {
+  circleId++;
+  crossID++;
+  let taskContent = document.createElement("div");
+  taskContent.setAttribute("class", "task-container");
+  taskContent.setAttribute("id", `task${crossID}`);
+  console.log(taskContent);
+  taskContent.innerHTML = `<div id="${circleId}" class="circle">
+                <img class="tick" src="images/tick.svg" alt=""></img>
                 </div>
                  <li class="task-value">${userTask.value}</li>
-                <img id="img${crossID}" class="cross" src="formkit--close.svg" alt="">
-            </div>`
-    
-            
-    conatiner.appendChild(taskContent);
-    let taskCompleted = document.querySelector('.circle');
-    console.log(taskCompleted);
-    userTask.value = ""
-    taskDone(taskContent)               
-})
+                <img id="img${crossID}" class="cross" src="images/formkit--close.svg" alt="">
+            </div>`;
 
-function taskDone(elem){
-    taskCompleted.addEventListener('click', ()=>{
-        if(taskCompleted === elem.target.id){
-            console.log("Circle is clicked")
-        }
-    })
-}
+  taskAdded.appendChild(taskContent);
+  console.log(taskAdded);
+
+
+  userTask.value = "";
+});
+
+taskAdded.addEventListener("click", (e) => {
+  if (e.target.classList.contains("cross")) {
+    const taskBox = e.target.closest(".task-container");
+
+    if (taskBox) {
+      taskBox.style.display = "none";
+    }
+  }
+  if (e.target.tagName === "LI") {
+    return;
+  }
+
+  if (e.target.classList.contains("tick")) {
+    const taskBox = e.target.closest(".task-container");
+    // console.log(taskBox);
+    if (taskBox) {
+      taskBox.style.display = "none";
+
+      const messageBox = document.querySelector(".message");
+      messageBox.textContent = "1 task completed";
+      messageBox.classList.add("show");
+
+      setTimeout(() => {
+        messageBox.classList.remove("show");
+      }, 2000);
+    }
+  }
+
+});
